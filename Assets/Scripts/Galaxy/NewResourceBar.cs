384b7486679db95737284cbf2d3755d2f83edc8e
cAlphaVictor <ciaran.vaille@gmail.com>
2023-01-07 18:12:12 -0600

    Began Work On A New Resource Bar

---
+++ Assets/Scripts/Galaxy/NewResourceBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewResourceBar : MonoBehaviour
+{
+    [Header("Image Components")]
+
+    [SerializeField] private Image flagBackgroundImage = null;
+    [SerializeField] private Image flagSymbolImage = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
